9c4c604ae4d41b989f5f206d4bd406dd8c2e4e3c
Muhammad Waseem <120702991+LuqmanAhmad786@users.noreply.github.com>
2024-01-21 03:45:11 +0000

    little bit about comparison operators

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,21 @@
+// console.log( 2 > 5);
+// console.log( 2 >= 5);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+// === it check stricly base value check with its datatypes
+
+console.log( "2" === 2)
