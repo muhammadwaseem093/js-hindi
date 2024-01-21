138795b2080e6a50a2ae29968c8b8ce0f96b50ed
Muhammad Waseem <120702991+LuqmanAhmad786@users.noreply.github.com>
2024-01-21 03:33:37 +0000

    little bit information about operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,15 +25,50 @@ let numberToBoolean = Boolean(example8)
 
 
 // print in console all records
-console.table([
-    numToString,
-    stringToNumber,
-    notKNown1,
-    example3,
-    notKnown2,
-    example4,
-    example5,
-    example6,
-    bigStringToNumber,
-    numberToBoolean
-])
+// console.table([
+//     numToString,
+//     stringToNumber,
+//     notKNown1,
+//     example3,
+//     notKnown2,
+//     example4,
+//     example5,
+//     example6,
+//     bigStringToNumber,
+//     numberToBoolean
+// ])
+
+
+
+// ************************** Opearions ***************************
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+// console.log( 2+2);
+// console.log( 2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello";
+let str2 = " pakistan";
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1",2)
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log( 1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+// gamecounter++
+++gamecounter
+console.log(gamecounter)
