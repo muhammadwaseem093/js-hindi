6e438a9b50189ede500f49780a987dfdcad62b70
Muhammad Waseem <120702991+LuqmanAhmad786@users.noreply.github.com>
2024-01-21 08:20:19 +0000

    some information about strings

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,14 @@
+const name = "waseem";
+const repoCount = 50;
+
+// console.log(name + repoCount + "value");
+
+console.log(`Hello my Name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String("8pool")
+
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))
+console.log(gameName.indexOf("l"))
