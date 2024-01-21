a6ccbafc2abfe2285fad1d280f4d62d8b22c570e
Muhammad Waseem <120702991+LuqmanAhmad786@users.noreply.github.com>
2024-01-21 02:43:09 +0000

    some informaton about data types with some comments details

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all js code as newer standard version in engine or it take in default if not write this statement
+
+// alert(3+3)  // we are using node js not in browsing
+
+
+let name = "waseem"
+let age = 28
+let isloggedIn = false
+let state = null;
+let temp ;
+// number =>  2 to the power 53
+// bigint 
+// string  => ""
+// boolean => true or false
+// null  =>  stand alone value , null is special datatype.
+// undefined => variable defined but not assigned any value.
+// symbol => uniqueness 
+
+
+// object
+
+console.log(typeof null)
