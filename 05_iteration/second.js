a0de71da12da8bb36241a5a5db53eb5dee6f8e8c
Muhammad Waseem <120702991+LuqmanAhmad786@users.noreply.github.com>
2024-01-22 13:33:48 +0000

    do while loop

---
+++ 05_iteration/second.js
@@ -0,0 +1,21 @@
+// while loop
+// let index = 0
+// while (index < 10) {
+//     console.log(`value of index is ${index}`);
+//     index++;
+    
+// }
+
+let myArray = ['waseem','irshad','akmal','murshid','mumtaz','mohsin','mubeen']
+
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr++;
+// }
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+
+    score++;
+} while (score <= 10);
