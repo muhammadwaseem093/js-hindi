90d230f5cd77b2003329b1d3cb8cd8e05a06883d
Muhammad Waseem <120702991+LuqmanAhmad786@users.noreply.github.com>
2024-01-22 07:12:35 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,17 @@
+// singleton with constructor
+
+// Object.create()
+// object literals
+
+const JsUser = {
+    name:"waseem",
+    age:29,
+    locaiton:"lahore",
+    email:"lahore@gmail.com",
+    isLoggedIn:false,
+    lastLoginDays: ["Monday","Saturday"]
+} 
+
+console.log(JsUser.email)
+console.log(JsUser['email'])
+
