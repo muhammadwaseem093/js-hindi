5e953f612cfc67e87bf753923496480788ae7557
Muhammad Waseem <120702991+muhammadwaseem093@users.noreply.github.com>
2025-02-05 04:24:18 +0000

    math

--- 01_basics/07_nums_maths.js
+++ 01_basics/07_nums_maths.js
@@ -20,7 +20,7 @@
 // ++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++==
 
 // console.log(Math);
-// console.log(Math.abs(4));
+// console.log(Math.abs(-4));
 // console.log(Math.round(4.3));
 // console.log(Math.ceil(4.2));
 // console.log(Math.floor(4.9));
